9cccd18177feb17124cc6900ee272ef6957d9bab
Aaron Geller <a-geller@northwestern.edu>
2022-08-16 15:25:58 -0500

    added interactive CMD tool to pipeline

--- getGaiaData/getGaiaData.py
+++ getGaiaData/getGaiaData.py
@@ -15,6 +15,15 @@ import matplotlib.cm as cm
 
 import yaml
 
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Button, Div, PointDrawTool, Slider, TextInput
+from bokeh.layouts import column, row
+from bokeh.io import curdoc
+from bokeh.transform import factor_cmap
+
+from shapely.geometry import LineString as shLs
+from shapely.geometry import Point as shPt
+
 class GaiaClusterMembers(object):
 	'''
 	This Class will grab data from the Gaia archive.  The user must provide the RA and Dec values,
@@ -561,7 +570,7 @@ class GaiaClusterMembers(object):
 								  np.nan_to_num(self.data['PPa'], nan=1)*\
 								  np.nan_to_num(self.data['PPM'].filled(), nan=1)
 
-	def plotCMD(self, data=None, x1='g_mean_psf_mag', x2='i_mean_psf_mag', y='g_mean_psf_mag', m='membership', savefig=True):
+	def plotCMD(self, data=None, color1='g_mean_psf_mag', color2='i_mean_psf_mag', mag='g_mean_psf_mag', mem='membership', savefig=True):
 		if (self.verbose > 0):
 			print("plotting CMD ...")
 
@@ -570,16 +579,19 @@ class GaiaClusterMembers(object):
 
 		# I could specify the columns to use
 		f, ax = plt.subplots(figsize=(5,8))
-		ax.plot(data[x1] - data[x2], data[y],'.', color='lightgray')
+		ax.plot(data[color1] - data[color2], data[mag],'.', color='lightgray')
+		ax.set_xlim(-1, 3)
+		ax.set_ylim(22, min(data[mag]))
+		ax.set_xlabel(color1 + ' - ' + color2, fontsize=16)
+		ax.set_ylabel(mag, fontsize=16)
 
 		#members
-		mask = (data[m] > self.membershipMin) 
-		ax.plot(data[mask][x1] - data[mask][x2], data[mask][y],'.', color='deeppink')
+		if (mem in data.columns):
+			mask = (data[mem] > self.membershipMin) 
+			ax.plot(data[mask][color1] - data[mask][color2], data[mask][mag],'.', color='deeppink')
+			ax.set_ylim(22, min(data[mask][mag]))
 
-		ax.set_ylim(22, min(data[mask][y]))
-		ax.set_xlim(-1, 3)
-		ax.set_xlabel(x1 + ' - ' + x2, fontsize=16)
-		ax.set_ylabel(y, fontsize=16)
+
 		if (savefig):
 			f.savefig(self.plotNameRoot + 'CMD.pdf', format='PDF', bbox_inches='tight')
 
@@ -617,7 +629,8 @@ class GaiaClusterMembers(object):
 		members['mass1'] = np.zeros(len(members)) + 1.1 #if we know masses, these could be added
 		members['massRatio'] = np.zeros(len(members)) #if we know mass ratios, these could be added
 		members['stage1'] = np.zeros(len(members)) + 1 #set to 1 for MS and giant stars (use 2(?) for WDs)
-		members['useDBI'] = np.zeros(len(members)) + 1 #set to 1 to use during burn-in.  May want to improve to remove anomalous stars
+		if ('useDBI' not in members.colnames):
+			members['useDBI'] = np.zeros(len(members)) + 1 #set to 1 to use during burn-in.  May want to improve to remove anomalous stars
 		out = members[['id', 
 					   'phot_g_mean_mag', 'phot_bp_mean_mag', 'phot_rp_mean_mag',
 					   'g_mean_psf_mag', 'r_mean_psf_mag', 'i_mean_psf_mag', 'z_mean_psf_mag', 'y_mean_psf_mag',
@@ -683,7 +696,7 @@ class GaiaClusterMembers(object):
 		# ffmt = '%-' + str(fdec + 3) + '.' + str(fdec) + 'f'
 		ffmt = '%-7.4f'
 		with open(filename, 'w', newline='\n') as f:
-			ascii.write(out, delimiter=' ', output=f, format = 'basic',
+			ascii.write(out, delimiter=' ', output=f, format = 'basic', overwrite=True,
 				formats = {'id': '%' + str(2*zfillN + 1) + 's', 
 						'G': ffmt, 'G_BP': ffmt, 'G_RP': ffmt, 
 						'g_ps': ffmt, 'r_ps': ffmt, 'i_ps': ffmt, 'z_ps': ffmt, 'y_ps': ffmt, 
@@ -753,6 +766,133 @@ class GaiaClusterMembers(object):
 
 
 
+
+
+	def createInteractive(self, mag = 'G', color1 = 'G_BP', color2 = 'G_RP', xrng = [0.5,2], yrng = [20,10]):
+		# NOTE: currently this code requires a column in the data labelled as 'membership'
+
+		# create the initial figure
+		TOOLS = "box_zoom, reset"
+		p = figure(title = "",
+			tools = TOOLS, width = 500, height = 700,
+			x_range = xrng, y_range = yrng)
+
+		self.data['useDBI'] = [0]*len(self.data)
+
+		mask = (self.data['membership'] > self.membershipMin) 
+
+		sourcePhot = ColumnDataSource(data = dict(x = self.data[mask][color1] - self.data[mask][color2], y = self.data[mask][mag]))
+		# empty for now, but will be filled below in updateStatus
+		sourcePhotSingles = ColumnDataSource(data = dict(x = [] , y = []))
+
+		# add the phot points to the plot
+		# Note: I could handle categorical color mapping with factor_cmap, but this does not seem to update in the callback when I change the status in sourcePhot (I removed status since this doesn't work)
+		# colorMapper = factor_cmap('status', palette = ['black', 'dodgerblue'], factors = ['unselected', 'selected'])
+		p.scatter(source = sourcePhot, x = 'x', y = 'y', alpha = 0.5, size = 3, marker = 'circle', color = 'black')
+		p.scatter(source = sourcePhotSingles, x = 'x', y = 'y', alpha = 0.75, size = 8, marker = 'circle', color = 'dodgerblue')
+
+		# add the PointDrawTool to allow users to draw points interactively
+		# to hold the user-added points
+		newPoints = ColumnDataSource(data = dict(x = [], y = []))   
+		renderer = p.circle(source = newPoints, x = 'x', y = 'y', color = 'limegreen', size = 10) 
+		drawTool = PointDrawTool(renderers = [renderer])
+		p.add_tools(drawTool)
+		p.toolbar.active_tap = drawTool
+
+		# add the line connecting the user-added points
+		p.line(source = newPoints, x = 'x', y = 'y', color = 'limegreen', width = 4)
+
+		# add filled polygon to hold the region within the selection zone for the plot
+
+		# callback to update the single-star selection when a point is added or when the slider changes
+		# https://stackoverflow.com/questions/47177493/python-point-on-a-line-closest-to-third-point
+
+		def updateStatus(attr, old, new):
+			if (len(newPoints.data['x']) > 1):
+				# define the user-drawn line using shapely
+				lne = shLs([ (x,y) for x,y in zip(newPoints.data['x'], newPoints.data['y']) ])
+				# find the distance for each point to the user-drawn line
+				# clear the singles data
+				data = dict(x = [] , y = [])
+				for i, row in enumerate(self.data):
+					if (mask[i]):
+						x = row[color1] - row[color2]
+						y = row[mag]
+						pte = shPt(x, y)
+						dst = pte.distance(lne)
+						# if the distance is within the user-defined tolerance, set the status to selected
+						self.data[i]['useDBI'] = 0
+						if (dst < slider.value):
+							self.data[i]['useDBI'] = 1
+							data['x'].append(x)
+							data['y'].append(y)
+				sourcePhotSingles.data = data
+		newPoints.on_change('data', updateStatus)
+
+		###########################
+		# widgets
+
+		# add a slider to define the width of the selection, next to the line
+		slider = Slider(start = 0, end = 0.1, value = 0.01, step = 0.001, format = '0.000', title = "Selection Region")
+		def sliderCallback(attr, old, new):
+			updateStatus(attr, old, new)
+		slider.on_change("value", sliderCallback)
+		
+		# add a reset button
+		resetButton = Button(label = "Reset",  button_type = "danger", )
+
+		def resetCallback(event):
+			newPoints.data = dict(x = [], y = [])
+			slider.value = 0.1  
+			self.data['useDBI'] = [0]*len(self.data)
+
+		resetButton.on_click(resetCallback)
+
+		# text box to define the output file
+		# outfile = TextInput(value = datafile + '.new', title = "Output File Name:")
+
+		# add a button to apply the filter
+		writeButton = Button(label = "Write .phot and .yaml files",  button_type = "success")
+
+		def writeCallback(event):
+			# output an updated phot file
+			# This will be improved when the code is combined with BASE9_utils
+			self.generatePhotFile()
+			self.generateYamlFile()
+			print('Files saved : ', self.photOutputFileName, self.yamlOutputFileName) 
+
+		writeButton.on_click(writeCallback)
+
+
+
+
+		###########################
+		# layout
+		# plot on the left, buttons on the right
+		buttons = column(
+			slider, 
+			Div(text='<div style="height: 15px;"></div>'),
+			#outfile,
+			writeButton,
+			Div(text='<div style="height: 15px;"></div>'),
+			resetButton,
+		)
+		title = 	Div(text='<div style="font-size:20px; font-weight:bold">Interactive CMD</div>')
+		instructions = 	Div(text='<ul style="font-size:14px">\
+			<li>Add points that define a line along the single-star sequence.</li>\
+			<li>To add points, first enable the "Point Draw Tool".</li>\
+			<li>Click to create a marker.  Click+drag any marker to move it.  Click+backspace any marker to delete it.</li>\
+			<li>Click the "Reset" button to remove all the points. </li>\
+			<li>When finished, click the "Apply Selection" button to select the single members.</li>\
+		</ul>')
+
+		layout = column(title, instructions, row(p, buttons))
+
+		return(layout)
+
+
+
+
 	def runAll(self):
 		self.getData()
 		self.getRVMembers()
@@ -760,6 +900,7 @@ class GaiaClusterMembers(object):
 		self.getPMMembers()
 		self.combineMemberships()
 		self.plotCMD()
+		# run the interactive?
 		self.generatePhotFile()
 		self.generateYamlFile()
 
